04c72bc19d61c89a0290be5b40e206e649984360
Hashim271 <hashim46@live.ca>
2025-04-22 17:17:48 -0400

    Added constructor that initializes the properties

---
+++ script.js
@@ -0,0 +1,7 @@
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name; // Name of Product (string)
+        this.price = price; // Price of Product (num)
+        this.quantity = quantity; // Stock Quantity (num)
+    }
+}
